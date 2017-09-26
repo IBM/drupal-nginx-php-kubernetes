551ce582878abd5e905fcc37c029e6ed9a766dd5
Daniel Krook <krook@us.ibm.com>
2017-09-26 12:47:22 -0400

    Resolved storage volume mapping.

--- scripts/docker/php-fpm/start.sh
+++ scripts/docker/php-fpm/start.sh
@@ -3,30 +3,31 @@ set -e
 
 # This is the mount point for the shared volume.
 # By default the mount point is owned by the root user.
-MOUNTPATH="/content/uploads"
-WWW_USER=${WWW_USER:-"www"}
+MOUNT_PATH="/content/uploads"
+TEMP_USER=${TEMP_USER:-"temp_user"}
 
 # This function creates a subdirectory that is owned by
 # the non-root user under the shared volume mount path.
 create_data_dir() {
   #Add the non-root user to primary group of root user.
-  usermod -aG root $WWW_USER
+  usermod -aG root $TEMP_USER
 
   # Provide read-write-execute permission to the group for the shared volume mount path.
-  mkdir -p $MOUNTPATH
-  chmod 775 $MOUNTPATH
+  chmod 777 $MOUNT_PATH
 
   # Create a directory under the shared path owned by non-root user www.
-  su -c "mkdir -p ${MOUNTPATH}/data" -l $WWW_USER
-  su -c "chmod 777 ${MOUNTPATH}/data" -l $WWW_USER
-  ls -al ${MOUNTPATH}
+  su -c "mkdir -p ${MOUNT_PATH}/data" -l $TEMP_USER
+  su -c "chmod -R 777 ${MOUNT_PATH}/data" -l $TEMP_USER
+  ls -al ${MOUNT_PATH}
 
   # For security, remove the non-root user from root user group.
-  deluser $WWW_USER root
+  deluser $TEMP_USER root
 
   # Change the shared volume mount path back to its original read-write-execute permission.
-  chmod 755 $MOUNTPATH
+  chmod 755 $MOUNT_PATH
   echo "Created Data directory..."
 }
 
 create_data_dir
+
+php-fpm
