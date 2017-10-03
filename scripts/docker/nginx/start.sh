044a7e5132b704551621636879c6683d7bb508a9
Daniel Krook <krook@us.ibm.com>
2017-10-03 17:45:42 -0400

    Adding Drupal deps to NGINX.

--- scripts/docker/nginx/start.sh
+++ scripts/docker/nginx/start.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Couldn't add this file at build time because the volume wasn't mounted
-cp /root/html/index.html /var/www/html/
+# cp /root/html/index.html /var/www/html/
 
 # Now that volume is usable, start up NGINX on port 80
 nginx -g "daemon off;";
