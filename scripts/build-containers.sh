c048ba5f71e4fb85e005ec89ed757ffe07fa6459
Daniel Krook <krook@us.ibm.com>
2017-10-13 11:01:48 -0400

    Fixed dependency issue.

--- scripts/build-containers.sh
+++ scripts/build-containers.sh
@@ -8,7 +8,8 @@ bx cr login
 
 # Build the NGINX image (configure Fast CGI)
 cd docker/nginx
-docker build --tag registry.ng.bluemix.net/jjdojo/nginx:latest --no-cache .
+# docker build --tag registry.ng.bluemix.net/jjdojo/nginx:latest --no-cache .
+docker build --tag registry.ng.bluemix.net/jjdojo/nginx:latest .
 docker push registry.ng.bluemix.net/jjdojo/nginx:latest
 
 # Move back to ROOT_DIR
@@ -16,7 +17,8 @@ cd $ROOT_DIR
 
 # Build the PHP-FPM image (base image, inject code, run composer)
 cd docker/php-fpm
-docker build --tag registry.ng.bluemix.net/jjdojo/php-fpm:latest --no-cache .
+# docker build --tag registry.ng.bluemix.net/jjdojo/php-fpm:latest --no-cache .
+docker build --tag registry.ng.bluemix.net/jjdojo/php-fpm:latest .
 docker push registry.ng.bluemix.net/jjdojo/php-fpm:latest
 
 # Move back to ROOT_DIR
@@ -24,6 +26,7 @@ cd $ROOT_DIR
 
 # Build the PHP-FPM image (base image, inject code, run composer)
 cd docker/php-cli
+# docker build --tag registry.ng.bluemix.net/jjdojo/php-cli:latest --no-cache .
 docker build --tag registry.ng.bluemix.net/jjdojo/php-cli:latest .
 docker push registry.ng.bluemix.net/jjdojo/php-cli:latest
 
