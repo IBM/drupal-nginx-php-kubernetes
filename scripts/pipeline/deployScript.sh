1a2d9f57395347bc37709faa67d2a5d6bd7c29be
odrodrig <odrodrig>
2017-10-17 18:22:29 -0500

    Changed build and deploy scripts to check for file changes

--- scripts/pipeline/deployScript.sh
+++ scripts/pipeline/deployScript.sh
@@ -3,12 +3,32 @@
 #Connect to a different container service API by uncommenting and specifying an API endpoint.
 #bx cs init --host https://us-south.containers.bluemix.net
 
-#Check cluster availability
-ip_addr=$(bx cs workers $PIPELINE_KUBERNETES_CLUSTER_NAME | grep normal | awk '{ print $2 }')
-if [ -z $ip_addr ]; then
-  echo "$PIPELINE_KUBERNETES_CLUSTER_NAME not created or workers not ready"
-  exit 1
-fi
+echo "Starting deploy stage"
 
-cd scripts/
-kubectl apply -f $IMAGE_NAME.yml
+DEPLOY_REQUIRED=$(grep "DEPLOY_REQUIRED" build.properties|cut -d'=' -f2)
+
+#Pulling image details
+IMAGE_NAME=$(grep "IMAGE_NAME" build.properties|cut -d'=' -f2)
+REGISTRY_URL=$(grep "REGISTRY_URL" build.properties|cut -d'=' -f2)
+REGISTRY_NAMESPACE=$(grep "REGISTRY_NAMESPACE" build.properties|cut -d'=' -f2)
+
+echo "$IMAGE_NAME"
+echo "$DEPLOY_REQUIRED"
+
+if [ "$DEPLOY_REQUIRED" = true ] ; then
+
+  echo "Changs have been made to images. Updating deployment"
+
+
+
+  #Applying configs
+  cd scripts/
+  #kubectl apply -f kubernetes/$IMAGE_NAME.yaml
+
+  kubectl set image deployment/$IMAGE_NAME $IMAGE_NAME="${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:latest"
+
+  kubectl rollout status deployment/$IMAGE_NAME
+
+else
+  echo "No change in ${IMAGE_NAME} container"
+fi
