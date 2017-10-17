9c8c85a0c5afc65dec4c6b01fe5e665237202c40
Daniel Krook <krook@us.ibm.com>
2017-10-17 16:55:58 -0400

    Work in progress on builds.

--- scripts/docker/nginx/start.sh
+++ scripts/docker/nginx/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# Couldn't add this file at build time because the volume wasn't mounted
+# Move any files now that the volume is mounted.
 # cp /root/html/index.html /var/www/html/
 
 # Now that volume is usable, start up NGINX on port 80
---
+++ scripts/pipeline/build-on-config-change.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+set -e
+
+# Inspect all the files in config and extract into env variables
+set -o allexport
+for file in ../../config/*.txt
+do
+  source $file
+done
+set +o allexport
+
+echo $DRUPAL_VERSION
+echo $DRUPAL_MD5
+echo $NGINX_VERSION
+echo $PHP_FPM_VERSION
+echo $PHP_CLI_VERSION
+
+# Build each image and push
+ROOT_DIR=`pwd`
+
+# Log into the Bluemix Container Registry
+bx cr login
+
+# Build the NGINX image (configure Fast CGI)
+cd ../docker/nginx
+docker build \
+  --tag registry.ng.bluemix.net/jjdojo/config-${DRUPAL_VERSION}-${NGINX_VERSION} \
+  --build-arg NGINX_VERSION=${NGINX_VERSION} \
+  --build-arg DRUPAL_VERSION=${DRUPAL_VERSION} \
+  --build-arg DRUPAL_MD5=${DRUPAL_MD5} \
+  --no-cache \
+  .
+docker push registry.ng.bluemix.net/jjdojo/config-${DRUPAL_VERSION}-${NGINX_VERSION}
+
+# Move back to ROOT_DIR
+cd $ROOT_DIR
+
+# Build the PHP-FPM image (base image, inject code, run composer)
+cd ../docker/php-fpm
+docker build \
+  --tag registry.ng.bluemix.net/jjdojo/config-${DRUPAL_VERSION}-${PHP_FPM_VERSION} \
+  --build-arg PHP_FPM_VERSION=${PHP_FPM_VERSION} \
+  --build-arg DRUPAL_MD5=${DRUPAL_MD5} \
+  --build-arg DRUPAL_VERSION=${DRUPAL_VERSION} \
+  --no-cache \
+  .
+docker push registry.ng.bluemix.net/jjdojo/config-${DRUPAL_VERSION}-${PHP_FPM_VERSION}
+
+# Move back to ROOT_DIR
+cd $ROOT_DIR
+
+# Build the PHP-FPM image (base image, inject code, run composer)
+cd ../docker/php-cli
+docker build \
+  --tag registry.ng.bluemix.net/jjdojo/config-${DRUPAL_VERSION}-${PHP_CLI_VERSION} \
+  --build-arg PHP_CLI_VERSION=${PHP_CLI_VERSION} \
+  --build-arg DRUPAL_MD5=${DRUPAL_MD5} \
+  --build-arg DRUPAL_VERSION=${DRUPAL_VERSION} \
+  --no-cache \
+docker push registry.ng.bluemix.net/jjdojo/config-${DRUPAL_VERSION}-${PHP_CLI_VERSION}
+
+# Move back to ROOT_DIR
+cd $ROOT_DIR
+
+# Lay down code on top of new base layers
+./build-on-code-change.sh
