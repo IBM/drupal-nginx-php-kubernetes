09316e5b88af4459c77ebc6d4c892f4b8a3ea0af
odrodrig <odrodrig>
2017-10-20 10:29:58 -0500

    Editing build/deploy script

---
+++ scripts/pipeline/build-on-code-change-2.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+set -e
+
+# Inspect all the files in config and extract into env variables
+set -o allexport
+for file in ../../code/*.txt
+do
+  source $file
+done
+
+for file in ../../config/*.txt
+do
+  source $file
+done
+set +o allexport
+
+BUILD_NUMBER=$( date +%s )
+
+echo $DRUPAL_VERSION
+echo $DRUPAL_MD5
+echo $DRUSH_VERSION
+echo $NGINX_VERSION     # Override from args
+echo $PHP_FPM_VERSION   # Override from args
+echo $PHP_CLI_VERSION   # Override from args
+
+# Build each image and push
+ROOT_DIR=`pwd`
+
+case "$IMAGE" in
+
+  "nginx")
+    # Build the NGINX image (configure Fast CGI)
+    cd ../docker/code-nginx
+    if [ -d tmp ]; then
+      rm -fr tmp
+    fi
+    mkdir tmp
+    cp -R ../../../code/ tmp/
+    docker build \
+      --tag registry.ng.bluemix.net/${REPOSITORY_NAMESPACE}/code-nginx:${BUILD_NUMBER} \
+      --tag registry.ng.bluemix.net/${REPOSITORY_NAMESPACE}/code-nginx:latest \
+      .
+    # --no-cache \
+    docker push registry.ng.bluemix.net/${REPOSITORY_NAMESPACE}/code-nginx:latest
+    rm -fr tmp
+
+    # Move back to ROOT_DIR
+    cd $ROOT_DIR
+    ;;
+
+  "php-fpm")
+    # Build the PHP-FPM image (base image, inject code, run composer)
+    cd ../docker/code-php-fpm
+    if [ -d tmp ]; then
+      rm -fr tmp
+    fi
+    mkdir tmp
+    cp -R ../../../code/ tmp/
+    docker build \
+      --tag registry.ng.bluemix.net/${REPOSITORY_NAMESPACE}/code-php-fpm:${BUILD_NUMBER} \
+      --tag registry.ng.bluemix.net/${REPOSITORY_NAMESPACE}/code-php-fpm:latest \
+      --build-arg DRUPAL_MD5=${DRUPAL_MD5} \
+      --build-arg DRUPAL_VERSION=${DRUPAL_VERSION} \
+      .
+    docker push registry.ng.bluemix.net/${REPOSITORY_NAMESPACE}/code-php-fpm:latest
+    rm -fr tmp
+
+    # Move back to ROOT_DIR
+    cd $ROOT_DIR
+    ;;
+
+  "php-cli")
+    # Build the PHP-CLI image (base image, inject code, run composer)
+    cd ../docker/code-php-cli
+    if [ -d tmp ]; then
+      rm -fr tmp
+    fi
+    mkdir tmp
+    cp -R ../../../code/ tmp/
+    docker build \
+      --tag registry.ng.bluemix.net/${REPOSITORY_NAMESPACE}/code-php-cli:${BUILD_NUMBER} \
+      --tag registry.ng.bluemix.net/${REPOSITORY_NAMESPACE}/code-php-cli:latest \
+      --build-arg DRUPAL_VERSION=${DRUSH_VERSION} \
+      .
+    docker push registry.ng.bluemix.net/${REPOSITORY_NAMESPACE}/code-php-cli:latest
+    rm -fr tmp
+
+    # Move back to ROOT_DIR
+    cd $ROOT_DIR
+    ;;
+esac
--- scripts/pipeline/deployScript.sh
+++ scripts/pipeline/deployScript.sh
@@ -7,53 +7,31 @@
 
 echo "Starting deploy stage"
 
-CONFIG_DEPLOY=$(grep "CONFIG_DEPLOY" build.properties|cut -d'=' -f2)
-CODE_DEPLOY=$(grep "CODE_DEPLOY" build.properties|cut -d'=' -f2)
+#Pulling image details
+IMAGE=$(grep "IMAGE" build.properties|cut -d'=' -f2)
+REGISTRY_URL=$(grep "REGISTRY_URL" build.properties|cut -d'=' -f2)
+REGISTRY_NAMESPACE=$(grep "REGISTRY_NAMESPACE" build.properties|cut -d'=' -f2)
 
-echo "$CONFIG_DEPLOY"
-echo "$CODE_DEPLOY"
 
-#Applying configs
-cd scripts/
+if [ kubectl get deployments | grep "${IMAGE}" ] ; then
 
-if [ "$CONFIG_DEPLOY" = true ] ; then
+  #Applying configs
+  cd scripts/
+  kubectl set image deployment/nginx nginx="${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE}:latest"
+  echo $?
 
-  kubectl set image deployment/config-nginx config-nginx="${REGISTRY_URL}/${REGISTRY_NAMESPACE}/config-nginx:latest"
   kubectl rollout status deployment/nginx
-
-  kubectl set image deployment/config-php-fpm config-php-fpm="${REGISTRY_URL}/${REGISTRY_NAMESPACE}/config-php-fpm:latest"
-  kubectl rollout status deployment/nginx
-
-  kubectl set image deployment/config-php-cli config-php-cli="${REGISTRY_URL}/${REGISTRY_NAMESPACE}/config-php-cli:latest"
-  kubectl rollout status deployment/nginx
-
-  kubectl set image deployment/code-nginx code-nginx="${REGISTRY_URL}/${REGISTRY_NAMESPACE}/code-nginx:latest"
-  kubectl rollout status deployment/code-nginx
-
-  kubectl set image deployment/code-php-fpm code-php-fpm="${REGISTRY_URL}/${REGISTRY_NAMESPACE}/code-php-fpm:latest"
-  kubectl rollout status deployment/code-php-fpm
-
-  kubectl set image deployment/code-php-cli code-php-cli="${REGISTRY_URL}/${REGISTRY_NAMESPACE}/code-php-cli:latest"
-  kubectl rollout status deployment/code-php-cli
+  echo $?
 
   kubectl get pods
-
-elif [ "$CODE_DEPLOY" = true ] ; then
-
-  kubectl set image deployment/code-nginx code-nginx="${REGISTRY_URL}/${REGISTRY_NAMESPACE}/code-nginx:latest"
-  kubectl rollout status deployment/code-nginx
-
-  kubectl set image deployment/code-php-fpm code-php-fpm="${REGISTRY_URL}/${REGISTRY_NAMESPACE}/code-php-fpm:latest"
-  kubectl rollout status deployment/code-php-fpm
-
-  kubectl set image deployment/code-php-cli code-php-cli="${REGISTRY_URL}/${REGISTRY_NAMESPACE}/code-php-cli:latest"
-  kubectl rollout status deployment/code-php-cli
-
-  kubectl get pods
-
 
 else
 
-  echo "No deployments needed"
+  cd scripts/
+  kubectl apply -f kubernetes/$IMAGE.yml
+
+  echo $?
+
+  kubectl get pods
 
 fi
