bc8fb198f76ee22c551437c58a1b63479155f4d3
AlexanderAllen <rallen36@its.jnj.com>
2017-10-24 23:35:44 -0400

    Build config images locally without pushing.

---
+++ scripts/k8-minikube/build/build-config-images.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -x
+
+ROOT_DIR=`pwd`
+
+echo "Building config images... "
+(
+  cd config-nginx
+  docker build --tag registry.ng.bluemix.net/jjdojo/nginx:latest .
+)
+(
+  cd config-php-fpm
+  docker build --tag registry.ng.bluemix.net/jjdojo/php-fpm:latest .
+)
+(
+  cd config-php-cli
+  docker build --tag registry.ng.bluemix.net/jjdojo/php-cli:latest .
+)
+
+echo "Build complete."
