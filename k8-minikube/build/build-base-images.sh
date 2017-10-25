471e4ace9b01479286beb477fc02cda273bbff97
AlexanderAllen <rallen36@its.jnj.com>
2017-10-25 03:35:07 -0400

    Oops, wrong rename, correct now.

--- k8-minikube/build/build-base-images.sh
+++ k8-minikube/build/build-base-images.sh
@@ -1,42 +1,17 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bash
+set -x
 
-# Inspect all the files in config and extract into env variables
-
-BUILD_NUMBER=$( date +%s )
-
-# Build each image and push
 ROOT_DIR=`pwd`
 
-# Build the NGINX image (configure Fast CGI)
+echo "Building base images... "
 
-# rallen: The nginx image should have no code whatsoever in it.
-# Whatever user code nginx sees should be on a volume, not on the docker image.
+cd nginx
+docker build --tag registry.ng.bluemix.net/jjdojo/nginx:latest .
 
-cd code-nginx
-docker build \
-  --tag registry.ng.bluemix.net/jjdojo/code-nginx:${BUILD_NUMBER} \
-  --tag registry.ng.bluemix.net/jjdojo/code-nginx:latest \
-  --build-arg NGINX_VERSION=1.13.5 \
-  .
+cd $ROOT_DIR/php-fpm
+docker build --tag registry.ng.bluemix.net/jjdojo/php-fpm:latest .
 
-# Build the PHP-FPM image (base image, inject code, run composer)
-cd $ROOT_DIR/code-php-fpm
+cd $ROOT_DIR/php-cli
+docker build --tag registry.ng.bluemix.net/jjdojo/php-cli:latest .
 
-# TODO: There's really nothing to build on the code php image.
-#docker build \
-#  --tag registry.ng.bluemix.net/jjdojo/code-php-fpm:${BUILD_NUMBER} \
-#  --tag registry.ng.bluemix.net/jjdojo/code-php-fpm:latest \
-#  .
-
-# Build the PHP-CLI image (base image, inject code, run composer)
-cd $ROOT_DIR/code-php-cli
-docker build \
-  --tag registry.ng.bluemix.net/jjdojo/code-php-cli:${BUILD_NUMBER} \
-  --tag registry.ng.bluemix.net/jjdojo/code-php-cli:latest \
-  .
-
-# Move back to ROOT_DIR
-cd $ROOT_DIR
-
-echo "Build completed."
+echo "Build complete."
