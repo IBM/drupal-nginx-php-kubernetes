a9240bb43cf6b082e1e2fc48a588c38d69df0ec1
AlexanderAllen <rallen36@its.jnj.com>
2017-10-25 02:27:48 -0400

    Update recreate script.

--- k8-minikube/recreate.sh
+++ k8-minikube/recreate.sh
@@ -2,6 +2,12 @@
 
 set +x
 
+# Start minikube.
+# minikube start
+
+# Connect Docker and kubectl to Minikube context.
+eval $(minikube docker-env)
+
 # Delete everything.
 kubectl delete deployment,service,rs --all
 
@@ -11,8 +17,15 @@ kubectl apply -f volumes -f pods-services
 # Show system status.
 kubectl get deployment,service,rs,pods
 
-# Show nginx endpoint.
+# Show nginx endpoint, automatically shows up when ready.
 minikube service nginx --url
 
 # Open default browser to Nginx
 minikube service nginx
+
+# Double check docker status, should show minikube containers.
+# docker ps
+
+# Nginx is now ready, start tailing it.
+# kubectl logs -f nginx
+# kubectl logs -f nginx-3049359248-5hvt9
