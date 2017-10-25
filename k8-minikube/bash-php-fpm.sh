092190513529fad3c822fcd9a9db36c109bdefd7
AlexanderAllen <rallen36@its.jnj.com>
2017-10-25 15:39:33 -0400

    Document Jsonpath and connecting to an individual container.

--- k8-minikube/bash-php-fpm.sh
+++ k8-minikube/bash-php-fpm.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
 kubectl exec -i -t $(kubectl get pod -l "app=php-fpm" -o jsonpath='{.items[0].metadata.name}') -- bash
+
+# Dump the Json so that you can narrow it down using Json Path:
+# kubectl get pod -l "app=php-fpm" -o=json
+
+# Example: the json path
+# .items[0].spec.containers[?(@.name=="php-cli-sidecar-phpfpm")].name
+
+# Example: get container name.
+# └─[$]> kubectl get pod -l "app=php-fpm" -o jsonpath='{.items[0].spec.containers[?(@.name=="php-cli-sidecar-phpfpm")].name}'
+# php-cli-sidecar-phpfpm
+
+# Connect to a particular container.
+# kubectl exec -i -t $(kubectl get pod -l "app=php-fpm" -o jsonpath='{.items[0].metadata.name}') -c php-cli-sidecar-phpfpm -- bash
