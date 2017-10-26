3b09ee8ecce7177c045102e4168e2bf178c6ecc2
AlexanderAllen <rallen36@its.jnj.com>
2017-10-26 18:49:18 -0400

    Update rsync script with env vars.

--- k8-minikube/build/app-image/app-sync.sh
+++ k8-minikube/build/app-image/app-sync.sh
@@ -3,8 +3,16 @@
 echo "App Image Sidecar Container"
 echo ""
 echo "Copying application into shared volume."
+echo ""
 
-# Todo, move over to rsync.
-# todo: check target dir exists or error out.
-# toto: use env var for source and target paths.
-cp -R /app /www-data
+echo "Rsync source is: ${RSYNC_SOURCE}/"
+echo "Rsync destination is: ${RSYNC_DEST}/"
+
+# Rsync to destination and preserve symlinks and file perms.
+rsync -vah "${RSYNC_SOURCE}/" "${RSYNC_DEST}"
+
+# Keep the container alive.
+echo ""
+echo "Finished sync job, keeping container alive..."
+echo ""
+php /root/noop.php
